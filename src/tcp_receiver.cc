2b0c705e6b323f6f04a6561c603ba8d351303709
mmh <miaominghao@qq.com>
2024-06-25 19:25:37 +0800

    done with check2

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -1,15 +1,54 @@
 #include "tcp_receiver.hh"
+#include "byte_stream.hh"
+#include "tcp_receiver_message.hh"
+#include "wrapping_integers.hh"
+#include <cstdint>
+#include <optional>
 
 using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message )
 {
-  // Your code here.
-  (void)message;
+  // if this message contains SYN
+  if (message.SYN) {
+    // set the zero_point
+    _zero_point = message.seqno;
+    // move to the right index for the first data
+    message.seqno = message.seqno + 1;
+    SYN = true;
+  }
+
+  // if this message contains no SIN or FIN and data, means error happend
+  if ( !message.SYN && !message.FIN && message.payload.empty() ) {
+    set_error();
+  }
+
+  // only if we've recived SYN, process the data
+  if (SYN) {  
+    uint64_t checkpoint = writer().bytes_pushed();
+    uint64_t first_index = message.seqno.unwrap(_zero_point, checkpoint) - 1; // not include SYN
+    // byte with invalid stream index should be ignored, which means the message.seqno == _zero_point.
+    if ( message.seqno == _zero_point) {
+      return;
+    }
+    _reassembler.insert(first_index, message.payload, message.FIN);
+  }
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
-  // Your code here.
-  return {};
+  std::optional<Wrap32> ackno;
+  // if we've not set the SYN, the ackno is empty
+  if (!SYN) {
+    ackno = nullopt;
+  } else {
+    ackno = Wrap32::wrap(writer().bytes_pushed() + 1, _zero_point); // include SYN
+    // if we assembled FIN message, which means the writer is closed, increase the ackno by 1 FIN
+    if (writer().is_closed()) {
+      ackno = ackno.value() + 1;
+    }
+  }
+  uint16_t window_size = writer().available_capacity() > UINT16_MAX ? UINT16_MAX : writer().available_capacity();
+  // use constructor to create a TCPReceiverMessage
+  return { ackno, window_size, has_error()};
 }
--- src/tcp_receiver.hh
+++ src/tcp_receiver.hh
@@ -3,12 +3,18 @@
 #include "reassembler.hh"
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
+#include "wrapping_integers.hh"
 
 class TCPReceiver
 {
 public:
   // Construct with given Reassembler
-  explicit TCPReceiver( Reassembler&& reassembler ) : reassembler_( std::move( reassembler ) ) {}
+  explicit TCPReceiver( Reassembler&& reassembler ) 
+  : _reassembler( std::move( reassembler ) ) 
+  , _zero_point( 0 )  // Initialize zero point to 0
+  , SYN( false ) // Initialize SYN to false
+  , RYN( false ) // Initialize RYN to false
+  {}
 
   /*
    * The TCPReceiver receives TCPSenderMessages, inserting their payload into the Reassembler
@@ -20,11 +26,20 @@ public:
   TCPReceiverMessage send() const;
 
   // Access the output (only Reader is accessible non-const)
-  const Reassembler& reassembler() const { return reassembler_; }
-  Reader& reader() { return reassembler_.reader(); }
-  const Reader& reader() const { return reassembler_.reader(); }
-  const Writer& writer() const { return reassembler_.writer(); }
+  const Reassembler& reassembler() const { return _reassembler; }
+  Reader& reader() { return _reassembler.reader(); }
+  const Reader& reader() const { return _reassembler.reader(); }
+  const Writer& writer() const { return _reassembler.writer(); }
+
+  // set error to streams
+  void set_error() { _reassembler.set_error(); }
+  // return true if has error in streams
+  bool has_error() const { return _reassembler.has_error(); }
+
 
 private:
-  Reassembler reassembler_;
+  Reassembler _reassembler;
+  Wrap32 _zero_point; // use for unwrap
+  bool SYN; // if set, means we've received a SYN from the peer
+  bool RYN; // if set, means error happend.
 };
